2177404e53bc7a29ac5a176b00a90f6a9c449fa8
Md Ashikur Rahman <ashikur.dev78@gmail.com>
2025-12-01 10:02:59 +0600

    feat(): add scribe for api documentation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+
+Route::get('/docs', function () {
+    $path = resource_path('views/scribe/index.blade.php');
+
+    if (!File::exists($path)) {
+        abort(404, 'Scribe docs not generated.');
+    }
+
+    return Response::make(view('scribe.index'));
 });
